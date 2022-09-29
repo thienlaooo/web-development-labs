e775905a58c5a862b2e4289c6bb6e9e6a24256bc
thienlaooo <olko.rysin28@gmail.com>
2022-09-29 16:57:37 +0300

    Second commit...

---
+++ execute.cmd
@@ -0,0 +1,2 @@
+curl http://127.0.0.1:8080/api/v1/hello-world-22
+curl -I http://127.0.0.1:8080/api/v1/hello-world-22
--- lab4.py
+++ lab4.py
@@ -1,1 +1,8 @@
-print('Hello World')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/hello-world-22")
+def hello_world():
+    return "Hello World 22"
