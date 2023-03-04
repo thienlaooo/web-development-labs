8c302e460ebd125e933ae4c471a8bb689f5a8b31
thienlaooo <olko.rysin28@gmail.com>
2023-03-04 20:06:33 +0200

    some changes

--- alembic/generateall.py
+++ alembic/generateall.py
@@ -3,7 +3,7 @@ from Models.Models import *
 from alembic.config import Config
 from alembic import command
 
-engine = create_engine("postgresql://postgres:admin@localhost:5432/Pharmacy")
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/pharmacy")
 Base.metadata.create_all(engine)
 alembic_cfg = Config("alembic.ini")
 command.stamp(alembic_cfg, "head")
